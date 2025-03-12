d494eb8723ac6f0496123ada55d2fd3f7bc09acd
No_Name <105019557+ah2355@users.noreply.github.com>
2025-03-12 09:15:15 -0400

    Update Star.java

--- src/game/Star.java
+++ src/game/Star.java
@@ -1,1 +1,24 @@
+package game;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.Random;
+
+public class Star {
+	private Point position;
+	
+	public Star(int width, int height) {
+		Random rand = new Random();
+		position = new Point(rand.nextDouble(width),rand.nextDouble(height));
+	}
+	
+	public void paint(Graphics brush) {
+		brush.setColor(Color.WHITE);
+		brush.fillOval((int) position.x, (int) position.y, 2, 2);
+	}
+	
+	public Point getPosition() {
+		return position;
+	}
+}
 
