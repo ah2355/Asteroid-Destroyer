b568dd599ae828edb3866651fc4d1be231f69fcb
No_Name <105019557+ah2355@users.noreply.github.com>
2025-03-11 20:30:03 -0400

    Update Ship.java

--- src/game/Ship.java
+++ src/game/Ship.java
@@ -1,9 +1,100 @@
 package game;
 
-public class Ship extends Element{
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+
+public class Ship extends Element implements Move{
+	private boolean forward = false;
+	private boolean left = false;
+	private boolean right = false;
+	private boolean backward = false;
 
 	public Ship(Point[] inShape, Point inPosition, double inRotation) {
 		super(inShape, inPosition, inRotation);
+	}
+	
+	public void paint(Graphics brush) {
+		brush.setColor(Color.RED);
+		super.paint(brush);
+	}
+	
+	public void move() {
+		double stepSize = 2.0;
+		
+		if(forward) {
+			double x = stepSize * Math.cos(Math.toRadians(rotation));
+			double y = stepSize * Math.sin(Math.toRadians(rotation));
+			
+			position.x += x;
+			position.y += y;
+		}
+		
+		if(backward) {
+			double x = stepSize * Math.cos(Math.toRadians(rotation));
+			double y = stepSize * Math.sin(Math.toRadians(rotation));
+			
+			position.x -= x;
+			position.y -= y;
+		}
+		
+		if(left) {
+			rotation -= 5;
+			if(rotation < 0) {
+				rotation += 360;
+			}
+		}
+		
+		if(right) {
+			rotation += 5;
+			if(rotation >= 360) {
+				rotation -= 360;
+			}
+		}
+		
+	}
+	
+	@Override
+	public void keyPressed(KeyEvent e) {
+		int key = e.getKeyCode();
+		
+		if(key == KeyEvent.VK_UP) {
+			forward = true;
+		}
+		
+		if(key == KeyEvent.VK_LEFT) {
+			left = true;
+		}
+		
+		if(key == KeyEvent.VK_RIGHT) {
+			right = true;
+		}
+		
+		if(key == KeyEvent.VK_DOWN) {
+			backward = true;
+		}
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+        int key = e.getKeyCode();
+		
+		if(key == KeyEvent.VK_UP) {
+			forward = false;
+		}
+		
+		if(key == KeyEvent.VK_LEFT) {
+			left = false;
+		}
+		
+		if(key == KeyEvent.VK_RIGHT) {
+			right = false;
+		}		
+		
+		if(key == KeyEvent.VK_DOWN) {
+			backward = false;
+		}
 	}
 
 }
