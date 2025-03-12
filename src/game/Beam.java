a79c5078ade2aec761d8a82a10a1f3676cab0069
No_Name <105019557+ah2355@users.noreply.github.com>
2025-03-12 07:46:19 -0400

    Update Beam.java

--- src/game/Beam.java
+++ src/game/Beam.java
@@ -6,36 +6,38 @@ import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 
 public class Beam {
-  private Point position;
-  private double rotation;
-  private double speed = 5.0;
-  private boolean active = true;
+	private Point position;
+	private double rotation;
+	private double speed = 5.0;
+	private boolean active = true;
+
+	public Beam(Point startPosition, double startRotation) {
+		position = new Point(startPosition.x, startPosition.y);
+		rotation = startRotation;
+	}
 
 
-  public Beam(Point startPosition, double startRotation){
-    position = new Point (startPosition.x, startPosition.y);
-    rotation = startRotation;
-  }
+	public void move() {
+		position.x += speed * Math.cos(Math.toRadians(rotation));
+		position.y += speed * Math.sin(Math.toRadians(rotation));
 
-  public void move(){
-    position.x += speed * Math.cos(Math.toRadians(rotation));
-    position.y += speed * Math.sin(Math.toRadians(rotation));
+		if (position.x < 0 || position.x > 800 || position.y < 0 || position.y > 600) {
+			active = false;
+		}
+	}
 
-    if (position.x < 0 || position.x > 800 || position.y < 0|| position.y > 600){
-      active = false;}
-    }
+	public void paint(Graphics brush) {
+		if (active) {
+			brush.setColor(Color.YELLOW);
+			brush.fillRect((int) position.x, (int) position.y, 5, 5);
+		}
+	}
 
-  public void paint(Graphics brush){
-    if(active){
-        brush.setColor(Color.YELLOW);
-        brush.fillRect((int) position.x, (int) position.y, 5, 5);}
-      }
+	public boolean isOnScreen() {
+		return active;
+	}
 
-  public boolean isOnScreen(){
-    return active;
-  }
-
-  public Point getPosition(){
-    return position;
-  }
-  }
+	public Point getPosition() {
+		return position;
+	}
+}
