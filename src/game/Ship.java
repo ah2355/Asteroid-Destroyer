991a9837c1e94ad973a1a7798e11b13db085c34e
No_Name <105019557+ah2355@users.noreply.github.com>
2025-03-11 21:17:57 -0400

    Update Ship.java

--- src/game/Ship.java
+++ src/game/Ship.java
@@ -4,95 +4,126 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 
-public class Ship extends Element implements Move{
+public class Ship extends Element implements Move {
 	private boolean forward = false;
 	private boolean left = false;
 	private boolean right = false;
 	private boolean backward = false;
+	private boolean shield = false;
+	private PowerUp power;
 
 	public Ship(Point[] inShape, Point inPosition, double inRotation) {
 		super(inShape, inPosition, inRotation);
 	}
-	
+
 	public void paint(Graphics brush) {
-		brush.setColor(Color.RED);
+		if (shield) {
+			brush.setColor(Color.CYAN);
+		} else {
+			brush.setColor(Color.RED);
+		}
 		super.paint(brush);
+		
+		if(power != null) {
+			power.powerE.check();
+		}
+		if(!power.powerE.isActive()) {
+			deactiveShield();
+			power = null;
+		}
 	}
 	
+	public void collectPower(PowerUp pw) {
+		power = pw;
+		power.activateShield(this);
+	}
+
+	public void activeShield() {
+		if (!shield) {
+			shield = true;
+		}
+	}
+
+	public void deactiveShield() {
+		if (shield) {
+			shield = false;
+		}
+	}
+
 	public void move() {
 		double stepSize = 2.0;
-		
-		if(forward) {
+
+		if (forward) {
 			double x = stepSize * Math.cos(Math.toRadians(rotation));
 			double y = stepSize * Math.sin(Math.toRadians(rotation));
-			
+
 			position.x += x;
 			position.y += y;
 		}
-		
-		if(backward) {
+
+		if (backward) {
 			double x = stepSize * Math.cos(Math.toRadians(rotation));
 			double y = stepSize * Math.sin(Math.toRadians(rotation));
-			
+
 			position.x -= x;
 			position.y -= y;
 		}
-		
-		if(left) {
+
+		if (left) {
 			rotation -= 5;
-			if(rotation < 0) {
+			if (rotation < 0) {
 				rotation += 360;
 			}
 		}
-		
-		if(right) {
+
+		if (right) {
 			rotation += 5;
-			if(rotation >= 360) {
+			if (rotation >= 360) {
 				rotation -= 360;
 			}
 		}
-		
+
 	}
-	
+
 	@Override
 	public void keyPressed(KeyEvent e) {
 		int key = e.getKeyCode();
-		
-		if(key == KeyEvent.VK_UP) {
+
+		if (key == KeyEvent.VK_UP) {
 			forward = true;
 		}
-		
-		if(key == KeyEvent.VK_LEFT) {
+
+		if (key == KeyEvent.VK_LEFT) {
 			left = true;
 		}
-		
-		if(key == KeyEvent.VK_RIGHT) {
+
+		if (key == KeyEvent.VK_RIGHT) {
 			right = true;
 		}
-		
-		if(key == KeyEvent.VK_DOWN) {
+
+		if (key == KeyEvent.VK_DOWN) {
 			backward = true;
 		}
-		
+
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-        int key = e.getKeyCode();
-		
-		if(key == KeyEvent.VK_UP) {
+		int key = e.getKeyCode();
+
+		if (key == KeyEvent.VK_UP) {
 			forward = false;
 		}
-		
-		if(key == KeyEvent.VK_LEFT) {
+
+		if (key == KeyEvent.VK_LEFT) {
 			left = false;
 		}
-		
-		if(key == KeyEvent.VK_RIGHT) {
+
+		if (key == KeyEvent.VK_RIGHT) {
 			right = false;
-		}		
-		
-		if(key == KeyEvent.VK_DOWN) {
+		}
+
+		if (key == KeyEvent.VK_DOWN) {
 			backward = false;
 		}
 	}
