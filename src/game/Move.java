e146519a325ebcb0bd026b98cffa7328005f6789
No_Name <105019557+ah2355@users.noreply.github.com>
2025-03-12 22:57:02 -0400

    Update Move.java

--- src/game/Move.java
+++ src/game/Move.java
@@ -1,5 +1,12 @@
 package game;
 
+/**
+ * INTERFACE: Move DESCRIPTION: Represents an object that can move within the
+ * game. Any class implementing this interface must define the move() method
+ */
 public interface Move {
-	
+	/**
+	 * Moves the implementing object based on its defined movement behavior.
+	 */
+	void move();
 }
